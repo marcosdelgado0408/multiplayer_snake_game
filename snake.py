f56a279ea81d8bf572f5f47218fd81492b980767
Marcos Delgado <marcosdelgado0408@gmail.com>
2019-10-09 15:24:49 -0300

    ajeitando para a segunda cobra existir

--- snake.py
+++ snake.py
@@ -33,8 +33,8 @@ class cube(object):
             radius = 3
             circleMiddle = (i*dis+centre-radius, j*dis+8)
             circleMiddle2 = (i*dis + dis - radius*2, j*dis+8)
-            pygame.draw.circle(surface, (0, 0, 0), circleMiddle, radius)
-            pygame.draw.circle(surface, (0, 0, 0), circleMiddle2, radius)
+            pygame.draw.circle(surface, (255, 255, 255), circleMiddle, radius)
+            pygame.draw.circle(surface, (255, 255, 255), circleMiddle2, radius)
 
 
 class snake(object):
@@ -43,7 +43,7 @@ class snake(object):
 
     def __init__(self, color, pos):
         self.color = color
-        self.head = cube(pos)
+        self.head = cube(pos, color)
         self.body.append(self.head)
         self.dirnx = 0
         self.dirny = 1
@@ -108,6 +108,96 @@ class snake(object):
         dx, dy = tail.dirnx, tail.dirny
 
         if dx == 1 and dy == 0:
+            self.body.append(cube((tail.pos[0]-1, tail.pos[1]),color=color))
+        elif dx == -1 and dy == 0:
+            self.body.append(cube((tail.pos[0]+1, tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(cube((tail.pos[0], tail.pos[1]-1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(cube((tail.pos[0], tail.pos[1]+1)))
+
+        self.body[-1].dirnx = dx
+        self.body[-1].dirny = dy
+
+    def draw(self, surface):
+        for i, c in enumerate(self.body):
+            if i == 0:
+                c.draw(surface, True)
+            else:
+                c.draw(surface)
+
+
+class snake2(object):
+    body = []
+    turns = {}
+
+    def __init__(self, color, pos):
+        self.color = color
+        self.head = cube(pos,color)
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 1
+
+    def move(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            keys = pygame.key.get_pressed()
+
+            for key in keys:
+                if keys[pygame.K_a]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_d]:
+                    self.dirnx = 1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_w]:
+                    self.dirnx = 0
+                    self.dirny = -1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_s]:
+                    self.dirnx = 0
+                    self.dirny = 1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+                if i == len(self.body)-1:
+                    self.turns.pop(p)
+            else:
+                if c.dirnx == -1 and c.pos[0] <= 0:
+                    c.pos = (c.rows-1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows-1:
+                    c.pos = (0, c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows-1:
+                    c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <= 0:
+                    c.pos = (c.pos[0], c.rows-1)
+                else:
+                    c.move(c.dirnx, c.dirny)
+
+    def reset(self, pos):
+        self.head = cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirnx = 0
+        self.dirny = 1
+
+    def addCube(self):
+        tail = self.body[-1]
+        dx, dy = tail.dirnx, tail.dirny
+
+        if dx == 1 and dy == 0:
             self.body.append(cube((tail.pos[0]-1, tail.pos[1])))
         elif dx == -1 and dy == 0:
             self.body.append(cube((tail.pos[0]+1, tail.pos[1])))
@@ -126,6 +216,10 @@ class snake(object):
             else:
                 c.draw(surface)
 
+
+
+
+
 '''
 def drawGrid(w, rows, surface):
     sizeBtwn = w // rows
@@ -141,9 +235,10 @@ def drawGrid(w, rows, surface):
 '''
 
 def redrawWindow(surface):
-    global rows, width, s, snack
+    global rows, width, s , s2, snack
     surface.fill((0, 0, 0))
     s.draw(surface)
+    s2.draw(surface)
     snack.draw(surface)
     #drawGrid(width, rows, surface)
     pygame.display.update()
@@ -176,12 +271,12 @@ def message_box(subject, content):
 
 
 def main():
-    global width, rows, s, snack
+    global width, rows, s,s2,  snack
     width = 500
     rows = 20
     win = pygame.display.set_mode((width, width))
-    s = snake((255, 0, 0), (10, 10))
-    s2 = snake((255, 0, 0), (10, 5))
+    s = snake((255, 0, 250), (10, 10))
+    s2 = snake2((0, 255, 0), (10, 5))
     snack = cube(randomSnack(rows, s), color=(0, 255, 0))
     flag = True
 
@@ -191,6 +286,7 @@ def main():
         pygame.time.delay(50)
         clock.tick(10)
         s.move()
+        s2.move()
         if s.body[0].pos == snack.pos:
             s.addCube()
             snack = cube(randomSnack(rows, s), color=(0, 255, 0))
