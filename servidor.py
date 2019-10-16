cd2da75b372ea519bb3beafe53ac90b49fbe1336
Marcos Delgado <marcosdelgado0408@gmail.com>
2019-10-16 19:14:26 -0300

    muitas modificações e testes

--- servidor.py
+++ servidor.py
@@ -1,70 +1,48 @@
 import socket
 import threading
 import socketserver
+ 
 
 
-global c1_msg, c2_msg
+def tratarCliente(clientsocket):
 
-c1_msg = ""
-c2_msg = ""
+        print("Servidor recebeu concexao de {}".format(adress))
+
+        msg_cliente = clientsocket.recv(1024).decode("utf-8")  # para transformar em string -> usar o decode
+
+        for i in range(0,len(lista_sockets)):
+            lista_sockets[i].send(bytes(msg_cliente,"utf-8"))        
+
+        print(msg_cliente)
+
+        #clientsocket.sendall(bytes(msg_cliente,"utf-8"))
+
+        clientsocket.close()  
+        lista_sockets.remove(clientsocket)
+
+
+
+
+
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind(("localhost",5555))
-s.listen()
+
+s.bind(("localhost",5556))
+print("Escutando...")
+s.listen(2)
+
+lista_sockets = []
 
 
-'''
-def tratarCliente(clientsocket):
-       
-    msg = "Mensagem recebida do servidor: "
-
-    msg_cliente  = clientsocket.recv(1024)
-
-    msg += msg_cliente.decode("utf-8") # para transformar em string -> usar o decode
-
-
-    clientsocket.sendall(bytes(msg,"utf-8"))
-
-    clientsocket.close();'''
-    
-
-    
-    
 
 while True:
-
     clientsocket, adress = s.accept()
-    
-    print("Servidor recebeu concexao de {}".format(adress))
-
-    msg_cliente = clientsocket.recv(1024)
-
-    # para transformar em string -> usar o decode
-    msg = msg_cliente.decode("utf-8")
-     
-
-    if(msg.find("c1") != -1): # caso encontre "c1" na mensagem do cliente
-        splited_msg = msg.split("-")
-        c1_msg = splited_msg[1]
-
-        clientsocket.sendall(bytes(c2_msg, "utf-8"))
-        clientsocket.close()
-
-
-    elif(msg.find("c2") != -1): # caso encontre "c2-" na mensagem do cliente
-        splited_msg = msg.split("-")
-        c2_msg = splited_msg[1]
-
-        clientsocket.sendall(bytes(c1_msg, "utf-8"))
-        clientsocket.close()
+    lista_sockets.append(clientsocket)
+    t = threading.Thread(target=tratarCliente,args=(clientsocket,)).start()
     
 
-    else:    
-        clientsocket.sendall(bytes(msg, "utf-8"))
 
-        clientsocket.close()
+
 
     
-'''    t = threading.Thread(target=tratarCliente,args=(clientsocket,))
-    t.start()
-'''
+    
